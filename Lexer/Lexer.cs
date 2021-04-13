720e71472bd79553d159b10a297fe9070b15d53a
Cameron <cameronproulx@outlook.com>
2021-04-12 21:45:02 -0400

    added print and fixed parentheses

--- Lexer/Lexer.cs
+++ Lexer/Lexer.cs
@@ -9,13 +9,7 @@ namespace Interpreter
     {
         public static IEnumerable<Token> Tokenize(string text)
         {
-            //remove white space
-            var characters = text
-                .ToCharArray()
-                .Where(x => x != ' ')
-                .ToArray();
-
-            using var reader = new StringReader(new string(characters));
+            using var reader = new StringReader(text);
 
             while (reader.Peek() != -1)
             {
@@ -24,23 +18,30 @@ namespace Interpreter
                 yield return character switch
                 {
                     '+' or '-' or '*' or '/' => new Token(TokenType.Operator, character.ToString()),
-                    ';' or '\n' => new Token(TokenType.NewLine, character.ToString()),
+                    ';' or '\n' or '\r' => new Token(TokenType.NewLine, character.ToString()),
 
                     '(' or '[' or '{' => new Token(TokenType.ParentheseOpen, character.ToString()),
                     ')' or ']' or '}' => new Token(TokenType.ParentheseClose, character.ToString()),
 
                     '=' => new Token(TokenType.Assign, character.ToString()),
 
-                    _ when char.IsLetter(character) => new Token(TokenType.Id, GetFullId(character, reader)),
+                    ' '  => new Token(TokenType.Discard, ""),
+                    _ when char.IsLetter(character) => HandleLetters(character, reader),
 
                     char digit when char.IsDigit(character) => new Token(TokenType.Integer, GetFullInteger(character, reader)),
                     _ => throw new Exception($"Charactor {character} not supported")
                 };
             }
 
-            static string GetFullId(char firstChar, StringReader reader)
+            Token HandleLetters(char firstChar, StringReader reader)
             {
-                return new string(Step().ToArray());
+                var word = new string(Step().ToArray());
+
+                return word switch
+                {
+                    "print" => new Token(TokenType.Print, word),
+                    _ => new Token(TokenType.Id, word)
+                };
 
                 IEnumerable<char> Step()
                 {
