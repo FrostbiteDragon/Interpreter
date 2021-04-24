dcaa7d424da82006befdf9c0f7b05a202039d2c9
Cameron <cameronproulx@outlook.com>
2021-04-23 22:57:46 -0400

    add var

---
+++ Parser/Statements/Var.cs
@@ -0,0 +1,16 @@
+﻿using FrostScript.Expressions;
+
+namespace FrostScript.Statements
+{
+    public class Var : Statement
+    {
+        public string Id { get; set; }
+        public Expression Value { get; set; }
+
+        public Var(string id, Expression value)
+        {
+            Id = id;
+            Value = value;
+        }
+    }
+}
