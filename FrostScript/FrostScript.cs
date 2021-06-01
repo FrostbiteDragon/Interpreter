eb5f6a1ff25bf4233975589715716b934a92aba2
Cameron <cameronproulx@outlook.com>
2021-06-01 15:30:31 -0400

    added a FrostScript class to easaly execute frostscript from .net

---
+++ FrostScript/FrostScript.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FrostScript.Expressions;
+using FrostScript.NativeFunctions;
+using FrostScript.Statements;
+using Frostware.Result;
+
+namespace FrostScript
+{
+    public static class FrostScript
+    {
+        public static void ExecuteString(string frostScript)
+        {
+            var tokens = Lexer.GetTokens(frostScript).ToArray();
+
+            Dictionary<string, IExpression> nativeFunctions = new()
+            {
+                ["print"] = new PrintFunction(),
+                ["clock"] = new ClockFunction()
+            };
+
+            if (Parser.GetAST(tokens, nativeFunctions) is Pass<IStatement[]> ast)
+            {
+                if (Interpreter.ExecuteProgram(ast.Value, nativeFunctions) is Fail)
+                    throw new Exception("Interpretation Failed");
+            }
+            else throw new Exception("Parsing failed");
+        }
+
+        public static T ExecuteString<T>(string frostScript)
+        {
+            var tokens = Lexer.GetTokens(frostScript).ToArray();
+
+            Dictionary<string, IExpression> nativeFunctions = new()
+            {
+                ["print"] = new PrintFunction(),
+                ["clock"] = new ClockFunction()
+            };
+
+            if (Parser.GetAST(tokens, nativeFunctions) is Pass<IStatement[]> ast)
+            {
+                if (Interpreter.ExecuteProgram(ast.Value, nativeFunctions) is Pass<object> programResult)
+                    return (T)programResult.Value;
+                else throw new Exception("Interpretation failed");
+            }
+            else throw new Exception("Parsing failed");
+        }
+
+        public static void ExecuteFile(string path)
+        {
+            var tokens = Lexer.GetTokens(File.ReadAllText(path)).ToArray();
+
+            Dictionary<string, IExpression> nativeFunctions = new()
+            {
+                ["print"] = new PrintFunction(),
+                ["clock"] = new ClockFunction()
+            };
+
+
+            if (Parser.GetAST(tokens, nativeFunctions) is Pass<IStatement[]> ast)
+            {
+                if (Interpreter.ExecuteProgram(ast.Value, nativeFunctions) is Fail)
+                    throw new Exception("Interpretation failed");
+            }
+            else throw new Exception("Parsing failed");
+        }
+
+        public static T ExecuteFile<T>(string path)
+        {
+            var tokens = Lexer.GetTokens(File.ReadAllText(path)).ToArray();
+
+            Dictionary<string, IExpression> nativeFunctions = new()
+            {
+                ["print"] = new PrintFunction(),
+                ["clock"] = new ClockFunction()
+            };
+
+            if (Parser.GetAST(tokens, nativeFunctions) is Pass<IStatement[]> ast)
+            {
+                if (Interpreter.ExecuteProgram(ast.Value, nativeFunctions) is Pass<object> programResult)
+                    return (T)programResult.Value;
+                else throw new Exception("Interpretation failed");
+            }
+            else throw new Exception("Parsing failed");
+        }
+    }
+}
