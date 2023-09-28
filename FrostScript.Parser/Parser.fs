46866adf76d801ed1c9207cec0e314cbfeb7ebde
Cameron <cameronproulx@outlook.com>
2023-09-28 11:16:25 -0400

    fixed parser token split

--- FrostScript.Parser/Parser.fs
+++ FrostScript.Parser/Parser.fs
@@ -7,10 +7,18 @@ module Parser =
             let current = source |> List.last
             source |> List.updateAt (source.Length - 1) (update current)
 
+        let x = 
+            tokens
+            |> List.fold (fun tokens token -> 
+                match token.Type with
+                | SemiColon -> List.append tokens [[]]
+                | _ -> tokens |> updateLast (fun lastTokenList -> List.append lastTokenList [token])
+            ) [[]]
+
         tokens
         |> List.fold (fun tokens token -> 
             match token.Type with
-            | SemiColon -> tokens |> List.append []
+            | SemiColon -> List.append tokens [[]]
             | _ -> tokens |> updateLast (fun lastTokenList -> List.append lastTokenList [token])
         ) [[]]
         |> List.map (fun tokens ->  
