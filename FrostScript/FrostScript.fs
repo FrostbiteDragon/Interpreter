84248acff5d1bcd3c1c731aa15938d4c31366617
Cameron <cameronproulx@outlook.com>
2024-05-28 17:30:54 -0400

    cleanup

--- FrostScript/FrostScript.fs
+++ FrostScript/FrostScript.fs
@@ -3,7 +3,9 @@
     open FrostScript.Domain
     open FrostScript.Features
 
-    let execute rawScript =
+    let execute =
+        let bindTraverse f input = Result.bind (fun x -> x |> List.traverseResult f) input
+
         let parse (input : Result<Token list list, (Token * string) list>) : Result<Node list, (Token * string) list> =
             let rec expression : ParseFunc = fun ctx ->
                 let ifNotEmpty onNotEmpty : ParseFunc = fun ctx -> if ctx.Tokens.IsEmpty then Ok ctx else onNotEmpty ctx
@@ -13,18 +15,18 @@
                 <| ctx
 
             input
-            |> Result.bind (fun tokenGroups -> tokenGroups |> List.traverseResult (fun tokens ->
+            |> bindTraverse (fun tokens ->
                 let ctx = { Tokens = tokens; Node = { Token = tokens.Head; Type = StatementNode } }
             
                 expression ctx
                 |> Result.map (fun ctx -> ctx.Node)
-            ))
+            )
 
         let validate (input : Result<Node list, (Token * string) list>) : Result<Expression list, (Token * string) list> =
             input
-            |> Result.bind (fun nodes -> nodes |> List.traverseResult (fun node -> 
+            |> bindTraverse (fun node -> 
                 let ctx = { Node = node; Ids = { Values = [] } }
                 choose [Literal.validate] ctx
-            ))
+            )
         
         parse >> validate
