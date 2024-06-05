eed0c58e1391144ac5b4420af2454588d22c53de
Cameron <cameronproulx@outlook.com>
2024-06-05 18:53:04 -0400

    namespace

--- FrostScript.Console/Program.fs
+++ FrostScript.Console/Program.fs
@@ -1,9 +1,10 @@
 ﻿open FrostScript
 open System.IO
+open System
 
-System.Environment.GetCommandLineArgs().[1]
+Environment.GetCommandLineArgs().[1]
 |> File.ReadAllText
 |> FrostScript.execute
 |> function
    | Ok v -> printf "%A" v
-   | Error e -> System.Console.WriteLine e
+   | Error e -> Console.WriteLine e
